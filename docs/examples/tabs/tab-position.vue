1b8147d403c6b171d2a80e351d3a247474639204
yao-six-yao <80759135+yao-six-yao@users.noreply.github.com>
2022-09-02 18:10:06 +0800

    Update tab-position.vue

--- docs/examples/tabs/tab-position.vue
+++ docs/examples/tabs/tab-position.vue
@@ -7,7 +7,7 @@
   </el-radio-group>
 
   <el-tabs :tab-position="tabPosition" style="height: 200px" class="demo-tabs">
-    <el-tab-pane label="User">User</el-tab-pane>
+    <el-tab-pane label="User" :disabled='false'>User</el-tab-pane>
     <el-tab-pane label="Config">Config</el-tab-pane>
     <el-tab-pane label="Role">Role</el-tab-pane>
     <el-tab-pane label="Task">Task</el-tab-pane>
